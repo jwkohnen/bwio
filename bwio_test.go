0ecff8f7a86c9b4427f6db2ee6b6b8416a0705d8
Johannes Kohnen <wjkohnen@users.noreply.github.com>
2016-12-27 00:30:15 +0100

    Add travis, hipster badges, update name and readme.

--- bwio_test.go
+++ bwio_test.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Wolfgang Johannes Kohnen <wjkohnen@users.noreply.github.com>
+ * Copyright (c) 2016 Johannes Kohnen <wjkohnen@users.noreply.github.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
